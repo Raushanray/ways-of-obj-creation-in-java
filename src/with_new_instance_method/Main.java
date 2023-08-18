53a658e804ab9809c0820f0f1ba3ddb35edaf698
Raushanray <raushankuray1998@gmail.com>
2023-08-18 17:25:47 +0530

    create an object using newInstance method..

---
+++ src/with_new_instance_method/Main.java
@@ -0,0 +1,19 @@
+package with_new_instance_method;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        //creating object using newInstance method of class Class..
+//        Student s2 = (Student) Class.forName("with_new_instance_method.Student").newInstance();
+
+        //2nd way
+        Student s2 = Student.class.newInstance();
+
+        //print the object ref to the console
+        System.out.println(s2);
+
+        //calling the student class property
+        s2.colg_id = 202;
+        System.out.println(s2.colg_id);
+    }
+}
---
+++ src/with_new_instance_method/Student.java
@@ -0,0 +1,10 @@
+package with_new_instance_method;
+
+public class Student {
+
+    public  int colg_id = 101;
+
+    public Student() {
+        System.out.println("Student class constructor ");
+    }
+}
